d02971b3f08c0f5a22d982230a33f558d168e497
KevinHu-1024 <hi-bai-du@163.com>
2016-01-24 11:41:58 +0800

    官网上面的bird系列页面

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+var birds = require('./birds');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+app.use('/birds', birds);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
